d96286e5fbbd47cf57c81bcebb66d6bad0eb4bdf
RobbieFaLL <153281526+RobbieFaLL@users.noreply.github.com>
2024-12-23 14:48:20 +0000

    ready for production

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dpgvzr#0apm0#rb5qy7ik6k0p75=0&y(82*$$%af*5$&h5a=_l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -133,6 +133,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Global static directory
 ]
+
+static_root = '/home/dstatic'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
