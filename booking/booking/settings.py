109b0f3350638f816dd7b19d1be4925e430268f5
Robbie <robertfall98@gmail.com>
2025-01-02 00:15:22 +0000

    on server now

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-dpgvzr#0apm0#rb5qy7ik6k0p75=0&y(82*$$%af*5$&h5a=_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dcoffee.robertfall.uk','www.dcoffee.robertfall.uk']
 
 # Application definition
 
